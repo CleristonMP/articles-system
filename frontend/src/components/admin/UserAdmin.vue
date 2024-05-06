c0cfd440abe29d15215c07164a3c0c03ef20765a
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2024-05-06 17:48:02 -0300

    Users table

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -1,12 +1,49 @@
 <template>
   <div class="user-admin">
-    <h1>Usuários Componente</h1>
+    <b-table hover striped :items="users" :fields="fields">
+      <template slot="actions" slot-scope="data">
+        <b-button variant="warning" @click="loadUser(data.item)" class="mr-2">
+          <i class="fa fa-pencil"></i>
+        </b-button>
+        <b-button variant="danger" @click="loadUser(data.item, 'remove')">
+          <i class="fa fa-trash"></i>
+        </b-button>
+      </template>
+    </b-table>
   </div>
 </template>
     
-    <script>
+<script>
+import { baseApiUrl } from "@/global";
+import axios from "axios";
+
 export default {
   name: "UserAdmin",
+  data: function () {
+    return {
+      mode: "save",
+      user: {},
+      users: [],
+      fields: [
+        { key: "id", label: "Código", sortable: true },
+        { key: "name", label: "Nome", sortable: true },
+        { key: "email", label: "E-mail", sortable: true },
+        { key: "admin", label: "Administrador", sortable: true, formatter: (value) => (value ? "Sim" : "Não"), },
+        { key: "actions", label: "Ações" },
+      ],
+    };
+  },
+  methods: {
+    loadUsers() {
+      const url = `${baseApiUrl}/users`;
+      axios.get(url).then((res) => {
+        this.users = res.data;
+      });
+    },
+  },
+  mounted() {
+    this.loadUsers();
+  },
 };
 </script>
     
