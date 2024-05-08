f962ab8187d3758dfb98727afaeb1084e6695d56
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2024-05-08 10:57:18 -0300

    Auth: (signing and signup)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,9 +10,6 @@ import router from "./config/router";
 
 Vue.config.productionTip = false;
 
-require("axios").defaults.headers.common["Authorization"] =
-  "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlNpbHZpbyBTYW50b3MiLCJlbWFpbCI6InNpbHZpb0BnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNzE1MDI3MTM2LCJleHAiOjE3MTUyODYzMzZ9.bMY7WuZYBe868c6s0LpSIO_Sg-E6TlK-zYaOFv9Lky8";
-
 new Vue({
   store,
   router,
